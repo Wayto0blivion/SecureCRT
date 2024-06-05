d8de876f6a41d71080ceac547923cc835dff82ba
Mitch <mitch.k@powerhouserecycling.com>
2024-06-05 09:14:32 -0400

    for loop issue still present, almost finished.

--- Juniper_Test.py
+++ Juniper_Test.py
@@ -30,8 +30,6 @@ objTab.Screen.IgnoreEscape = True
 end_line = chr(13)
 confirmation = 'y' + end_line
 
-
-
 # Variables for EX4550
 krnlPrompt = "/kernel"
 ldPrompt = "loader>"
@@ -41,42 +39,64 @@ pwPrompts = ["password:", "New password:", "Retype new password:", "Password:"]
 prompts = ["root#", "root>", "root@", "root@:RE:0%"]
 lgnPrompt = "login:"
 
-# # Sends 2 "spaces" to interrupt normal boot
-# objTab.Screen.WaitForString(krnlPrompt)
-# objTab.Screen.Send(" ")
-# objTab.Screen.Send(" ")
-#
-# # Wait for loader prompt and boot into safemode
-# objTab.Screen.WaitForString(ldPrompt)
-# objTab.Screen.Send("boot -s" + end_line)
-#
-# # Looks for /bin prompt and send recovery command
-# objTab.Screen.WaitForString(binPrompt)
-# objTab.Screen.Send("recovery" + end_line)
-#
-# # Loop for error handling.
-# while True:
-#     screen_output = objTab.Screen.ReadString(error, 30)
-#     if screen_output:
-#         objTab.Screen.Send("continue" + end_line)
-#     else:
-#         break
-#
-# # Start system zeroize
-# objTab.Screen.WaitForStrings(prompts)
-# objTab.Screen.Send("request system zeroize" + end_line)
-# objTab.Screen.Send(confirmation)
+# Sends 2 "spaces" to interrupt normal boot
+objTab.Screen.WaitForString(krnlPrompt)
+objTab.Screen.Send(" ")
+objTab.Screen.Send(" ")
+
+# Wait for loader prompt and boot into safemode
+objTab.Screen.WaitForString(ldPrompt)
+objTab.Screen.Send("boot -s" + end_line)
+
+# Looks for /bin prompt and send recovery command
+objTab.Screen.WaitForString(binPrompt)
+objTab.Screen.Send("recovery" + end_line)
+
+# Loop for error handling.
+while True:
+    screen_output = objTab.Screen.ReadString(error, 30)
+    if screen_output:
+        objTab.Screen.Send("continue" + end_line)
+    else:
+        break
+
+# Start system zeroize
+objTab.Screen.WaitForStrings(prompts)
+objTab.Screen.Send("request system zeroize" + end_line)
+objTab.Screen.Send(confirmation)
+# objTab.Screen.WaitForString(lgnPrompt)
 
 # List of commands to finish wiping unit
 strings = [
+    "root",
     "cli",
     "edit",
     "set system root-authentication plain-text-password",
+    " ",
     "delete virtual-chassis",
     "delete chassis auto-image-upgrade",
     "commit",
     "exit"]
 
+for string in strings:
+    if string is "set system root-authentication plain-text-password":
+        objTab.Screen.Send("set system root-authentication plain-text-password" + end_line)
+        pw = objTab.Screen.WaitForStrings(pwPrompts)
+        objTab.Screen.Send("juniper1" + end_line)
+        rpw = objTab.Screen.WaitForStrings(pwPrompts)
+        objTab.Screen.Send("juniper1" + end_line)
+    if string is "root":
+        wait = objTab.Screen.WaitForStrings(lgnPrompt)
+        objTab.Screen.Send("root" + end_line)
+
+    else:
+        objTab.Screen.Send(string + end_line)
+        prompt = objTab.Screen.WaitForStrings(prompts)
+
+# View PID/Hardware status
+objTab.Screen.Send("show chassis hardware" + end_line)
+
+# =======================================================================================================
 # Iterate over list to confirm password.
 # for Index, string in enumerate(strings):
 #     if Index == 3:
@@ -117,20 +137,3 @@ strings = [
 # log_message(str(objTab.Screen.WaitForStrings(pwPrompts)))
 # objTab.Screen.Send("juniper1" + end_line)
 # log_message(str(objTab.Screen.WaitForStrings(prompts)))
-
-
-for string in strings:
-    if string is "set system root-authentication plain-text-password":
-        objTab.Screen.Send("set system root-authentication plain-text-password" + end_line)
-        pw = objTab.Screen.WaitForStrings(pwPrompts)
-        objTab.Screen.Send("juniper1" + end_line)
-        rpw = objTab.Screen.WaitForStrings(pwPrompts)
-        objTab.Screen.Send("juniper1" + end_line)
-
-    else:
-        objTab.Screen.Send(string + end_line)
-        prompt_ = objTab.Screen.WaitForStrings(prompts)
-
-
-# View PID/Hardware status
-objTab.Screen.Send("show chassis hardware" + end_line)
